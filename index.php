3b43516aa6c9b15185fd04235307dfc154d35bd1
Your Name <you@example.com>
2019-11-25 17:16:20 +0100

    my commitXD

---
+++ index.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="utf-8">
+    <title>Zwierzętexpol LTD.</title>
+    <link rel="stylesheet" type="text/css" href="styl.css">
+</head>
+
+<body>
+    <div class="baner">
+        <img src="logo.png" class="logo">
+    </div>
+    <container class="kontener">
+        <section class="left">
+            <!-- Miejsce na skrypt -->
+        </section>
+
+        <section class="center">
+        </section>
+
+        <section class="right">
+        </section>
+
+    </container>
+    <section class="footer">
+        Witrynę wykonał Kaciej Mania
+    </section>
+</body>
